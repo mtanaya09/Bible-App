de9c91199fffa538f543232035391a2fda49174b
Michael <mtanaya09@gmail.com>
2022-04-21 14:09:05 +0800

    myplans

--- screens/planScreen.js
+++ screens/planScreen.js
@@ -4,72 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import MyPlans from "../PlansButton/MyPlans";
-
-// function MyPlans() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//         backgroundColor: "#ffffff",
-//       }}
-//     >
-//       <Text style={{ fontSize: 20, color: "black", fontWeight: "800" }}>
-//         My Plans
-//       </Text>
-
-//       <Text
-//         style={{
-//           fontSize: 15,
-//           color: "black",
-//           fontWeight: "800",
-//           marginTop: 10,
-//           padding: 10,
-//         }}
-//       >
-//         'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-//         commodo ligula eget dolor. Aenean massa.'
-//       </Text>
-//     </View>
-//   );
-// }
-
-function FindPlans() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#ffffff",
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 20,
-          color: "black",
-          fontWeight: "800",
-        }}
-      >
-        Find Plans
-      </Text>
-
-      <Text
-        style={{
-          fontSize: 15,
-          color: "black",
-          fontWeight: "800",
-          marginTop: 10,
-          padding: 10,
-        }}
-      >
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-        commodo ligula eget dolor. Aenean massa.'
-      </Text>
-    </View>
-  );
-}
+import FindPlans from "../PlansButton/FindPlans";
 
 const Tab = createMaterialTopTabNavigator();
 
